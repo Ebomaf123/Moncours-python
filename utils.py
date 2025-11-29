1646703f8005d70831dacd2d8dd557dcf4e0c69c
Sharif <sharifnikiema5@gmail.com>
2025-11-29 03:14:10 +0100

    exercises/6_1_Modules: implement and import utils module

---
+++ utils.py
@@ -0,0 +1,10 @@
+# utils.py (Ceci est le MODULE)
+import math
+
+def calculate_area_circle(radius):
+    """Calcule l'aire d'un cercle."""
+    return math.pi * radius**2
+
+def calculate_perimeter_square(side):
+    """Calcule le périmètre d'un carré."""
+    return 4 * side
